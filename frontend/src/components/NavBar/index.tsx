ca806f3f9a3e0d73a8eec798a6314256217bea56
Thiago Lima Santos <tlima7601@gmail.com>
2026-05-18 00:39:03 -0300

    arrumando a navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -10,21 +10,20 @@ export function NavBar({ role }: NavBarProps) {
   const { user, logout } = useAuth();
 
   return (
-    <nav className={styles.home}>
-      <h1 className={styles.title}>{role}</h1>
-
-      <div className={styles.userContainer}>
-        <div className={styles.userInfo}>
-          <p className={styles.userName}>Olá, {user?.name}</p>
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>{role}</h1>
+        <div className={styles.userContainer}>
+          <div className={styles.userInfo}>
+            <p className={styles.userName}>Olá, {user?.name}</p>
+          </div>
+          <button className={styles.avatar}>
+            <HiOutlineUser />
+          </button>
+          <button onClick={() => logout()} className={styles.logout}>
+            <HiOutlineLogout size={25} />
+          </button>
         </div>
-
-        <button className={styles.avatar}>
-          <HiOutlineUser />
-        </button>
-
-        <button onClick={() => logout()} className={styles.logout}>
-          <HiOutlineLogout size={25} />
-        </button>
       </div>
     </nav>
   );
